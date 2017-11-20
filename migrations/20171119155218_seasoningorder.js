ac932160ae43d2c14efebfcc6e817cf6f25bf45b
Dane Louis Bratz <danebratz@gmail.com>
2017-11-19 19:25:49 -0800

    stuff

--- migrations/20171119155218_seasoningorder.js
+++ migrations/20171119155218_seasoningorder.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex) {
-    knex.schema.alterTable('recipe_seasonings', function(table) {
+    return knex.schema.alterTable('recipe_seasonings', function(table) {
         table.integer('present_order');
     });
 };
 
 exports.down = function(knex) {
-    knex.schema.alterTable('recipe_seasonings', function(table) {
+    return knex.schema.alterTable('recipe_seasonings', function(table) {
         table.dropColumn('present_order');
     });
 };
