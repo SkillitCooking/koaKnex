fba4d3c698498014b09bd934ef31486f36e0bed9
Dane Louis Bratz <danebratz@gmail.com>
2017-11-20 17:38:08 -0800

    yo

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -194,7 +194,7 @@ module.exports = {
                     console.log('here!', seasoningsToUpdate);
                     return ctx.app.db.transaction(async function(trx) {
                         let updates = await getUpdateQueries(ctx.app.schemas.recipeSeasonings, seasoningsToUpdate, trx('recipe_seasonings'));
-                        return Promise.all(updates);
+                        await Promise.all(updates);
                     });
                 })();
             } catch (e) {
