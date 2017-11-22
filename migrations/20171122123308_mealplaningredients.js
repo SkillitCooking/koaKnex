1783ee0dbada626cf5459fc6422a0f3643708256
Dane Louis Bratz <danebratz@gmail.com>
2017-11-22 13:52:23 -0800

    oopos

--- migrations/20171122123308_mealplaningredients.js
+++ migrations/20171122123308_mealplaningredients.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
     return knex.schema
         .createTable('meal_plan_ingredients', function(table) {
             table.uuid('id').unique().primary().notNullable();
-            table.uuid('meal_plan').notNullable().references('meal_plans.id').onDelete('CASCASDE');
+            table.uuid('meal_plan').notNullable().references('meal_plans.id').onDelete('CASCADE');
             table.uuid('ingredient').notNullable().references('ingredients.id').onDelete('CASCADE');
             table.unique(['meal_plan', 'ingredient']);
             table.timestamps(true, true);
